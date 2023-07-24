c0cf1477845987bd30d87ecaa43fc392239fa1f4
chemwang13 <chemwang13@naver.com>
2023-07-24 14:48:52 +0900

    [S09P12A708-180 #11] feat: SNS댓글 작성 기능 구현

--- server/src/main/generated/com/eightlow/decalcomanie/common/mapper/BaseMapperImpl.java
+++ server/src/main/generated/com/eightlow/decalcomanie/common/mapper/BaseMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-07-24T14:43:43+0900",
+    date = "2023-07-24T14:48:32+0900",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 11 (Oracle Corporation)"
 )
 @Component
