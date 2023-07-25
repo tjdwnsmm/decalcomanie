c40159423e5bb5310fd4de364787ccba239d97b7
kimssumin <ksumin0730@naver.com>
2023-07-25 15:45:14 +0900

    [S09P12A708-276 #14] feat: 추천 향수 부분 구현

---
+++ client/src/components/Main/RecommendPerfume.tsx
@@ -0,0 +1,84 @@
+import { styled } from 'styled-components';
+
+const perfumeInfo = {
+  brand: '딥디크',
+  name: '로파피에',
+  scent: ['미모사', '베르가못', '머스크'],
+  img: 'src/assets/img/perfume1.png',
+};
+const RecommendPerfume = () => {
+  return (
+    <PerfumeInfoBox>
+      <Image>
+        <img src={perfumeInfo.img}></img>
+      </Image>
+      <InfoBox>
+        <Brand>{perfumeInfo.brand}</Brand>
+        <Info>
+          <PerfumeName>{perfumeInfo.name}</PerfumeName>
+          <PerfumeScent>
+            {perfumeInfo.scent.map((scent, idx) =>
+              idx === perfumeInfo.scent.length - 1 ? (
+                <>{scent}</>
+              ) : (
+                <>{scent}, </>
+              ),
+            )}
+          </PerfumeScent>
+        </Info>
+      </InfoBox>
+    </PerfumeInfoBox>
+  );
+};
+
+export default RecommendPerfume;
+
+const PerfumeInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const InfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 70px;
+  margin-top: 11px;
+`;
+
+const Brand = styled.div`
+  font-size: 11px;
+  font-weight: 600;
+  margin-bottom: 5px;
+`;
+
+const Info = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 251px;
+  align-items: center;
+`;
+
+const PerfumeName = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+const PerfumeScent = styled.div`
+  font-size: 13px;
+  font-weight: 400;
+`;
+
+const Image = styled.div`
+  width: 251px;
+  height: 251px;
+  border-radius: 10px;
+  background-color: var(--white-color);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 39px auto 0;
+  img {
+    width: 130px;
+  }
+`;
--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Main } from '../../style';
 import MainRecommend from '../../components/Main/MainRecommend';
+import RecommendPerfume from '../../components/Main/RecommendPerfume';
 
 const MainPage = () => {
   const [isDrawer, setDrawer] = useState(true);
@@ -10,6 +11,7 @@ const MainPage = () => {
       {isDrawer ? (
         <>
           <MainRecommend />
+          <RecommendPerfume />
         </>
       ) : (
         <></>
