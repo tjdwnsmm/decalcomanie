b5ea18b2d293f8033fed850146263fc6ae043514
kimssumin <ksumin0730@naver.com>
2023-08-02 09:52:37 +0900

    [#28 S09P12A708-308] style: 로그인화면 디자인 수정

--- client/src/pages/MainPage/LoginPage.tsx
+++ client/src/pages/MainPage/LoginPage.tsx
@@ -1,6 +1,7 @@
 import { styled } from 'styled-components';
 import { Main, MarginFrame } from '../../style';
 import { RESTAPI_KEY } from '../../api/apikey';
+import Logo from '../../components/common/Logo';
 
 const LoginPage = () => {
   const redirect_uri = 'http://localhost:8080/oauth/kakao/callback'; //Redirect URI
@@ -14,23 +15,32 @@ const LoginPage = () => {
   };
 
   return (
-    <Main>
-      <MarginFrame margin="100px 0">
+    <MainInLogin>
+      <MarginFrame margin="280px 0 0">
+        <Logo />
+      </MarginFrame>
+      <MarginFrame margin="550px 0 0">
         <KakaoBox onClick={kakaoLogin}>
           <img id="icon-img" src="src/assets/img/kakao_logo.png" />
           카카오톡으로 로그인
         </KakaoBox>
       </MarginFrame>
-    </Main>
+    </MainInLogin>
   );
 };
 
 export default LoginPage;
 
+const MainInLogin = styled(Main)`
+  background-color: var(--secondary-color);
+`;
+
 const KakaoBox = styled.div`
-  width: 100%;
+  width: 340px;
   height: 44px;
+  margin: auto;
   margin-bottom: 8px;
+  border-radius: 5px;
   font-size: 15px;
   font-weight: bold;
   color: #000000cb;
