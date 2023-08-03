3df4a30639e34b63c6fb8fe175e9732dc7e2ed50
tjdwnsmm <tjdwnsmm@naver.com>
2023-08-03 14:25:29 +0900

    refactor: userId accessToken에서 추출

--- server/src/main/java/com/eightlow/decalcomanie/user/controller/UserApiController.java
+++ server/src/main/java/com/eightlow/decalcomanie/user/controller/UserApiController.java
@@ -1,10 +1,12 @@
 package com.eightlow.decalcomanie.user.controller;
 
 import com.eightlow.decalcomanie.perfume.dto.PerfumeDto;
+import com.eightlow.decalcomanie.perfume.entity.Perfume;
 import com.eightlow.decalcomanie.user.dto.FollowDto;
 import com.eightlow.decalcomanie.user.dto.UserPerfumeDto;
 import com.eightlow.decalcomanie.user.dto.response.FollowerResponse;
 import com.eightlow.decalcomanie.user.dto.response.FollowingResponse;
+import com.eightlow.decalcomanie.user.entity.UserPerfume;
 import com.eightlow.decalcomanie.user.mapper.UserPerfumeMapper;
 import com.eightlow.decalcomanie.user.service.IUserService;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -27,33 +30,34 @@ public class UserApiController {
 
     // 사용자 향수 등록, 삭제
     @PostMapping("/perfume/manage")
-    public ResponseEntity<String> modifyUserPerfume(@RequestBody UserPerfumeDto request, HttpServletRequest req) {
-        String userMessage = userService.modifyUserPerfume(userPerfumeMapper.toEntity(request));
+    public ResponseEntity<String> modifyUserPerfume(@RequestBody Map<String, Integer> request, HttpServletRequest req) {
+        UserPerfume userPerfume = new UserPerfume((String)req.getAttribute("userId"), request.get("perfumeId"));
+        String userMessage = userService.modifyUserPerfume(userPerfume);
         return new ResponseEntity<>(userMessage, HttpStatus.CREATED);
     }
 
     // 사용자 향수 조회
-    @GetMapping("/perfume/{userId}")
-    public ResponseEntity<List<PerfumeDto>> getUserPerfume(@PathVariable String userId, HttpServletRequest req) {
-        return new ResponseEntity<>(userService.getUserPerfume(userId), HttpStatus.OK);
+    @GetMapping("/perfume")
+    public ResponseEntity<List<PerfumeDto>> getUserPerfume(HttpServletRequest req) {
+        return new ResponseEntity<>(userService.getUserPerfume((String)req.getAttribute("userId")), HttpStatus.OK);
     }
 
     // 팔로우
     @PostMapping("/follow")
-    public ResponseEntity<String> followUser(@RequestBody FollowDto request, HttpServletRequest req) {
-        return new ResponseEntity<>(userService.followUser(request.getFollowing(), request.getFollowed()), HttpStatus.CREATED);
+    public ResponseEntity<String> followUser(@RequestBody Map<String, String> request, HttpServletRequest req) {
+        return new ResponseEntity<>(userService.followUser((String)req.getAttribute("userId"), request.get("to")), HttpStatus.CREATED);
     }
 
     // 팔로잉 목록 조회
-    @GetMapping("/following/{userId}")
-    public ResponseEntity<List<FollowingResponse>> getFollowingUsers(@PathVariable String userId, HttpServletRequest req) {
-        return new ResponseEntity<>(userService.getFollowingUsers(userId), HttpStatus.OK);
+    @GetMapping("/following")
+    public ResponseEntity<List<FollowingResponse>> getFollowingUsers(HttpServletRequest req) {
+        return new ResponseEntity<>(userService.getFollowingUsers((String)req.getAttribute("userId")), HttpStatus.OK);
     }
 
     // 팔로우 목록 조회
-    @GetMapping("/follower/{userId}")
-    public ResponseEntity<List<FollowerResponse>> getFollowers(@PathVariable String userId, HttpServletRequest req) {
-        return new ResponseEntity<>(userService.getFollowers(userId), HttpStatus.OK);
+    @GetMapping("/follower")
+    public ResponseEntity<List<FollowerResponse>> getFollowers(HttpServletRequest req) {
+        return new ResponseEntity<>(userService.getFollowers((String)req.getAttribute("userId")), HttpStatus.OK);
     }
 
 }
