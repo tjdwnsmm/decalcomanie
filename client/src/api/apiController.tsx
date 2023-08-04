8efb7989b4229189b96c5ead95e1212770c9ed09
kimssumin <ksumin0730@naver.com>
2023-08-04 10:39:56 +0900

    [S09P12A708-353 #31] fix: APIController 상태코드 따라 수정

--- client/src/api/apiController.tsx
+++ client/src/api/apiController.tsx
@@ -37,7 +37,7 @@ instance.interceptors.request.use(
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
-    console.log(config);
+    // console.log(config);
     return config;
   },
   (error) => Promise.reject(error),
@@ -48,28 +48,47 @@ instance.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
-    console.log(error.response);
+    console.log(error.response, error.response.data.status, getRefreshToken());
     //401 에러면 refresh token 보내기
-    if (error.response.status === 401 && getRefreshToken()) {
-      const navigate = useNavigate();
+    if (error?.response?.data?.status === 401) {
+      console.log('access-token 만료됐어');
       try {
-        const response = await axios.post(`/oauth/reissue`, {
-          refreshToken: getRefreshToken(),
-        });
+        console.log('refresh-token 보낼게!');
+        const response = await axios.get(
+          `http://localhost:8080/oauth/reissue`,
+          {
+            headers: {
+              refreshToken: getRefreshToken(),
+            },
+          },
+        );
+
+        // 응답 헤더에서 Access Token과 Refresh Token 추출
+        const accessToken = response.headers['accesstoken'];
+        const refreshToken = response.headers['refreshtoken'];
 
         //access token 을 다시 setting 하고 origin request 를 재요청
-        setAccessToken(response.data.accessToken);
-        setRefreshToken(response.data.refreshToken);
-        originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+        setAccessToken(accessToken);
+        setRefreshToken(refreshToken);
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+
+        //새로운 토큰 발급 확인
+        console.log(accessToken, refreshToken);
+
         return axios(originalRequest);
       } catch (error) {
         // 만약 refreshToken 보내도 error 가 뜨면 login 화면으로 보내기 -> redirect
         console.log('Error refreshing token:', error);
 
-        navigate('/login'); // 로그인화면으로 보내기
+        window.location.href = '/login'; // 로그인화면으로 보내기
         localStorage.removeItem('accessToken');
         localStorage.removeItem('refreshToken');
       }
+    } else if (error?.response?.data?.status === 403) {
+      //access token 자체를 안보낸 경우
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('refreshToken');
+      window.location.href = '/login';
     }
 
     return Promise.reject(error);
