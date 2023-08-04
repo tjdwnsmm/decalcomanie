26570dd7b1e30a49945e51a30a6b8c2c30393774
chemwang13 <chemwang13@naver.com>
2023-08-04 17:39:54 +0900

    develop-back 브랜치 복구

---
+++ server/src/main/java/com/eightlow/decalcomanie/sns/dto/response/Response.java
@@ -0,0 +1,10 @@
+package com.eightlow.decalcomanie.sns.dto.response;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+public class Response {
+	private String message;
+}
