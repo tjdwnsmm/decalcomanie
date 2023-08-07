a3d3ede1d171ae5ed037b0f7f2f294cb254e54d4
이승민 <0283esm@naver.com>
2023-08-07 09:42:40 +0900

    [S09P12A708-389 #35 hotfix: entity table 매핑 수정

--- server/src/main/java/com/eightlow/decalcomanie/auth/entity/UserCredential.java
+++ server/src/main/java/com/eightlow/decalcomanie/auth/entity/UserCredential.java
@@ -5,12 +5,14 @@ import lombok.*;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
+@Table(name = "usercredential")
 public class UserCredential {
     @Id
     @Column(name="userId")
