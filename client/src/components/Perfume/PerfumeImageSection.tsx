b5ee0eb4300219b8475c09f0d99780544c19bb02
kimssumin <ksumin0730@naver.com>
2023-08-10 13:28:49 +0900

    style: 향수 상세페이지 성별정보 출력 변경

--- client/src/components/Perfume/PerfumeImageSection.tsx
+++ client/src/components/Perfume/PerfumeImageSection.tsx
@@ -7,27 +7,49 @@ interface PerfumeImageSectionProps {
   perfume: PerfumeDetail;
 }
 
+const gender = ['For men', 'For women', 'For unisex'];
 const PerfumeImageSection: React.FC<PerfumeImageSectionProps> = ({
   perfume,
 }) => {
   return (
     <InfoSection>
-      <PerfumeImg>
+      <PerfumeImg gender={perfume.gender}>
         <img src={perfume.picture} alt="Perfume" />
       </PerfumeImg>
-      <MoreInfo longevity={perfume.longevity} sillage={perfume.sillage} />
+      <Info>
+        <Gender>{gender[perfume.gender]}</Gender>
+        <MoreInfo longevity={perfume.longevity} sillage={perfume.sillage} />
+      </Info>
     </InfoSection>
   );
 };
 
 export default PerfumeImageSection;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
 const InfoSection = styled.div`
   display: flex;
   margin: 30px 20px;
 `;
+const Gender = styled.div`
+  width: fit-content;
+  height: fit-content;
+  background-color: var(--primary-color);
+  color: var(--white-color);
+  margin: 3px -25px -2px 0px;
+  padding: 3px 6px;
+  text-align: center;
+  border-radius: 4px;
+  font-size: 14px;
+`;
 
-const PerfumeImg = styled.div`
+const PerfumeImg = styled.div<{ gender: number }>`
   display: flex;
+  flex-direction : column;
   align-items: center;
   justify-content: center;
   width: 100px;
