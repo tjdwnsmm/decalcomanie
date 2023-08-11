93ffd7dbcb641510b533ecdc7d28874294b97ef7
이승민 <0283esm@naver.com>
2023-08-11 10:16:03 +0900

    [S09P12A708-412 #46] fix: kakao login debuging

--- server/src/main/java/com/eightlow/decalcomanie/auth/controller/OAuthController.java
+++ server/src/main/java/com/eightlow/decalcomanie/auth/controller/OAuthController.java
@@ -141,6 +141,7 @@ public class OAuthController {
         UUID userId = UUID.randomUUID();
 
         String accessToken = jwtService.generateAccessToken(kakaoProfile.getId().toString(), userId.toString());
+        System.out.println("accessToken: "+ accessToken);
         String refreshToken = jwtService.generateRefreshToken(kakaoProfile.getId().toString(), userId.toString());
 
         userCredential = UserCredential.builder()
