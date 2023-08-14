83fb962de64ab8fe25cede9eaadbd8e7694153e6
tjdwnsmm <tjdwnsmm@naver.com>
2023-08-14 15:45:58 +0900

    refactor: 향수 검색 결과에 마지막 페이지 여부 같이 반환

---
+++ server/src/main/java/com/eightlow/decalcomanie/perfume/dto/response/SearchResponse.java
@@ -0,0 +1,14 @@
+package com.eightlow.decalcomanie.perfume.dto.response;
+
+import com.eightlow.decalcomanie.perfume.dto.PerfumeDto;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder(toBuilder = true)
+public class SearchResponse {
+    boolean lastPage;
+    List<PerfumeDto> searchedPerfumes;
+}
