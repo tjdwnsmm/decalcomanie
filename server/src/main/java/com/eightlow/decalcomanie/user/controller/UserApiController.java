ca6c193a6e3a25e02f0092b046cbf701ecb1f60c
이승민 <0283esm@naver.com>
2023-08-14 15:29:46 +0900

    [S09P12A708-415 #47] feat: 향수 추천 api

--- server/src/main/java/com/eightlow/decalcomanie/user/controller/UserApiController.java
+++ server/src/main/java/com/eightlow/decalcomanie/user/controller/UserApiController.java
@@ -37,6 +37,7 @@ public class UserApiController {
     @PostMapping("/perfume/manage")
     public ResponseEntity<String> modifyUserPerfume(@RequestBody Map<String, Integer> request, HttpServletRequest req) {
         String userMessage = userService.modifyUserPerfume((String)req.getAttribute("userId"), request.get("perfumeId"));
+        userService.recommendUserPerfume((String)req.getAttribute("userId"));
         return new ResponseEntity<>(userMessage, HttpStatus.CREATED);
     }
 
@@ -100,11 +101,6 @@ public class UserApiController {
         return new ResponseEntity<>(userService.getUserPerfumeRecommend((String)req.getAttribute("userId")), HttpStatus.OK);
     }
 
-    // 사용자 향수 추천 로직 실행
-    @GetMapping("/recommend/update")
-    public ResponseEntity<List<PerfumeDto>> recommendUpdate(HttpServletRequest req) {
-        return new ResponseEntity<>(userService.recommendUserPerfume((String)req.getAttribute("userId")), HttpStatus.OK);
-    }
 
     // 닉네임 중복검사
     @GetMapping("/update/check/{nickname}")
